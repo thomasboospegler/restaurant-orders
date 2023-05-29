feb749e8610af78e0f830839c38cdf015d3d7dbb
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-29 09:37:21 -0400

    test: add tests for dishes class

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,28 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    dish1 = Dish("feijoada", 10.00)
+    dish2 = Dish("feijoada", 10.00)
+    dish3 = Dish("strogonoff", 15.00)
+    rice = Ingredient("arroz")
+    meat = Ingredient("carne")
+    dish1.add_ingredient_dependency(rice, 2)
+    dish1.add_ingredient_dependency(meat, 3)
+    assert dish1.name == "feijoada"
+    assert dish1.__eq__(dish3) is False
+    assert dish1.__eq__(dish2) is True
+    assert repr(dish1) == "Dish('feijoada', R$10.00)"
+    assert hash(dish1) == hash(dish2)
+    assert hash(dish1) != hash(dish3)
+    assert dish1.get_ingredients() == {
+        Ingredient("arroz"), Ingredient("carne")}
+    assert dish1.get_restrictions() == {
+        Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED}
+    with pytest.raises(ValueError):
+        Dish("feijoada", 0)
+    with pytest.raises(TypeError):
+        Dish("feijoada", "value")
