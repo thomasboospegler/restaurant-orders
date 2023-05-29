3d7f8657df47594d3289158a0a9af7c95e6e963f
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-29 09:19:11 -0400

    test: add tests for ingredient class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,11 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("cebola")
+    ingredient2 = Ingredient("pimenta")
+    assert ingredient1 != ingredient2
+    assert ingredient1 == ingredient1
+    assert repr(ingredient1) == "Ingredient('cebola')"
+    assert hash(ingredient1) == hash("cebola")
+    assert ingredient1.name == "cebola"
+    assert ingredient1.restrictions == set()
